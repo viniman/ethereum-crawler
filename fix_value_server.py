04a3265e347004729520c6b59fae472590b838be
viniman <vinicius.carlos@live.com>
2020-07-21 09:12:31 -0300

    Finaliza script de atualizaçao do value no banco de dados

--- fix_value_server.py
+++ fix_value_server.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import os
-
-#from sqlalchemy import create_engine
-#engine = create_engine('sqlite://', echo=False)
-
 import psycopg2
 
+
+'''
+    Atualiza dado de value das transações que tiveram o gas já atualizado com outro crawler
+'''
 
 database = {
     "host": "localhost",
@@ -18,15 +18,17 @@ database = {
 
 
 def concatenate_datasets():
-    path = 'dataset_fix/'
-    all_files = os.path.join(path, "*.csv")
-
+    
+    all_files = get_files_path()
     df = pd.concat((pd.read_csv(file_name) for file_name in all_files))
 
     print(df.count())
 
     return df
 
+def get_files_path(path='dataset_fix/'):
+    all_files = os.path.join(path, "*.csv")
+    return all_files
 
 def update_transactions(df):
     connection = psycopg2.connect(host=database['host'], database=database['db'],
@@ -34,23 +36,28 @@ def update_transactions(df):
     cursor = connection.cursor()
 
     for index, tx in df.iterrows():
-        print(index)
+        print(index, end=' ')
 
-        sql_instruction = "UPDATE {0} SET value = \'{1}\', gas = \'{2}\', tx_updated = true WHERE hash =\'{4}\';".format(
-            database['table'], tx['value'], tx['gas'], tx['hash']
+        sql_instruction = "UPDATE {0} SET value = \'{1}\' WHERE hash =\'{4}\';".format(
+            database['table'], tx['value']
         )
 
-    cursor.execute(sql_instruction)
-    connection.commit()
+        cursor.execute(sql_instruction)
+        connection.commit()
 
 
-def main():
-    
-    df = concatenate_datasets()
-    df.to_csv('dataset_entire_fix.csv', index=False)
+
+
+'''
+pega o caminho para os arquivos csv
+atualiza as transações de cada arquivo
+'''
+files_path = get_files_path()
+for index, file_path in zip(range(0,len(files_path)), files_path):
+    print('-'*50)
+    print ('CSV', index)
+    df = pd.read_csv(file_path)
     update_transactions(df)
 
 
-    
-if __name__ == '__main__':
-   main() 
+
