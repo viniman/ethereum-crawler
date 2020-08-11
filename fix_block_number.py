123adb3cd3c43c0faa5de58e99c78952de60bf5e
viniman <vinicius.carlos@live.com>
2020-08-11 09:40:13 -0300

    Ajusta script que atualiza block_numbers nulos

--- fix_block_number.py
+++ fix_block_number.py
@@ -1,7 +1,13 @@
+import psycopg2
 import urllib.request
+import json
+from random import shuffle
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
+
+    def get_data_from_url(self, url):
+        return self.open(url).read().decode()
 
 
 # ETHERSCAN KEYS
@@ -12,7 +18,18 @@ api_keys = ['YZVQYCEXE9FC7HEG22YE2S4X8CGG6QX86G',
             '53KDADTQF4TKHXFJMQEKYUYXDEZ4F75Q8Y',
             'ABGJSQIDI2UMGJ89IIGEKA5N1WDQ98Z3CI']
 
+
 counter = 0
+
+def get_api_key():
+    global api_keys
+    global counter
+    key_round = counter
+    key_round = key_round + 1
+    counter = key_round % 6
+    shuffle(api_keys)
+    return str(api_keys[counter])
+
 
 
 table = 'transactions4'
@@ -20,7 +37,7 @@ table = 'transactions4'
 
 opener = AppURLopener()
 
-def get_transaction_block_number(self, tx_hash) -> Transaction:
+def get_transaction_block_number(tx_hash):
     global opener
 
     url = 'https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash={}&apikey={}'.format(
@@ -37,6 +54,10 @@ with psycopg2.connect(host="localhost", database="ethscan", user="postgres", pas
 
     cur = conn.cursor()
 
+    count = "select count(hash) from transactions4 where receipt_status is not null and block_number is null and tx_updated".format(table)
+    cur.execute(count)
+    print(cur.fetchall())
+
     transactions_hash = "select hash from transactions4 where receipt_status is not null and block_number is null and tx_updated".format(table)
     cur.execute(transactions_hash)
 
@@ -50,8 +71,8 @@ with psycopg2.connect(host="localhost", database="ethscan", user="postgres", pas
         
         try:
             block_number = get_transaction_block_number(hash)
-            
-            sql = "UPDATE {} SET receipt_status = '{}' WHERE hash = '{}'".format(table, block_number, hash)
+
+            sql = "UPDATE {} SET block_number = '{}' WHERE hash = '{}'".format(table, int(block_number, 16), hash)
             print(sql, block_number)
             cur.execute(sql)
             conn.commit()
